e6a0e269d343fd9c422dbac8e843f4d94777d2cb
Alfandy <ezaalfandy.ea@gmail.com>
2017-02-16 05:44:45 +0700

    project

---
+++ asp_mvc_2/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asp_mvc_2.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+
+    public ActionResult LogIn()
+    {
+        return View();
+    }
+    [HttpPost]
+    public ActionResult LogIn(UserLoginView ULV, string returnUrl)
+    {
+        if (ModelState.IsValid)
+        {
+            UserManager UM = new UserManager();
+            string password = UM.GetUserPassword(ULV.LoginName);
+            if (string.IsNullOrEmpty(password))
+                ModelState.AddModelError("", "The user login or password provide is incorrect.");
+else {
+                if (ULV.Password.Equals(password))
+                {
+                    FormsAuthentication.SetAuthCookie(ULV.LoginName, false);
+                    return RedirectToAction("Welcome", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The password provided is incorrect.");
+                }
+            }
+        }
+        // If we got this far, something failed, redisplay form
+        return View(ULV);
+    }
+}
